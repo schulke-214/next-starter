9866f1bbc37cb7744a930516bd60745ad2bdc66f
schulke-214 <schulkemaximilian@gmail.com>
2019-09-21 22:38:10 +0200

    build: add more folders to the template

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const directory = (dir = '') => path.join(process.cwd(), dir);
 
 module.exports = {
 	webpack: config => {
-		config.resolve.alias['lib'] = directory('lib');
 		config.resolve.alias['components'] = directory('components');
 		config.resolve.alias['functions'] = directory('functions');
-		config.resolve.alias['layout'] = directory('layout');
+		config.resolve.alias['lib'] = directory('lib');
+		config.resolve.alias['pages'] = directory('pages');
 		config.resolve.alias['~'] = directory();
 
 		config.plugins = config.plugins || [];
