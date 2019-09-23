fc0f841f9d9f5f74fdd4917257f0e6c62e6a1a9e
schulke-214 <schulkemaximilian@gmail.com>
2019-09-23 19:47:30 +0200

    refactor: throw away scss and migrate global styles to styled-components

---
+++ styles/themes/default.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+	colors: {
+		primary: '#fff',
+		secondary: '#000'
+	},
+	spacings: {},
+	typography: {}
+};
+
+export default theme;
