f9795fc43ac42bcce63ad5e70c5c876161eb7df5
schulke-214 <schulkemaximilian@gmail.com>
2019-09-24 23:10:49 +0200

    feat: normalize styles

--- styles/main.ts
+++ styles/main.ts
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'polished';
 
 export default createGlobalStyle`
+	${normalize()}
+
 	* {
-		margin: 0;
-		padding: 0;
-		color: ${props => props.theme.colors.primary}
+		color: ${props => props.theme.colors.primary};
 	}
 `;
