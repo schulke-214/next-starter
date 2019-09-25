e504cd76c22e8b279d7b155efc73bdf12265ea2d
schulke-214 <schulkemaximilian@gmail.com>
2019-09-25 20:20:24 +0200

    build: update the setup script

--- config/setup.sh
+++ config/setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# initialization
+function handle-err {
+	if [ -f ".setup-log" ]; then
+		echo "err: something went wrong. take a look at the .setup-log file"
+		exit 1
+	fi
+}
+
+rm .setup-log
 
 echo "init/git-flow"
-if ! git flow init 2>./.setup-log; then
-	echo "err: git flow failed to initialize - see ./.setup-log" >&2
-	exit 1
-fi
+git flow init 2>.setup-log
+handle-err
 
 echo "init/node"
-nvm install 2>./.setup-log;
-nvm use 2>./.setup-log;
+nvm install 2>.setup-log
+nvm use 2>.setup-log
+handle-err
+
 yarn --silent
+handle-err
 
 echo "init/env"
 cp config/default.env .env
+handle-err
