6512454b034c096b59010a4de3cfbcc5419ae062
schulke-214 <schulkemaximilian@gmail.com>
2019-10-20 22:02:38 +0200

    fix: remove apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,29 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { ApolloProvider } from 'react-apollo';
 import { ThemeProvider } from 'styled-components';
 
 import Theme from 'styles/themes/default';
 import GlobalStyles from 'styles/main';
 
-import withApolloClient from 'lib/apollo/with-apollo-client';
-
-interface Props {
-	apollo: {
-		client: any;
-	};
-}
+interface Props {}
 
 class Application extends App<Props> {
 	render() {
-		const {
-			Component,
-			pageProps,
-			apollo: { client }
-		} = this.props;
+		const { Component, pageProps } = this.props;
 
 		return (
 			<ThemeProvider theme={Theme}>
-				<ApolloProvider client={client}>
+				<>
 					<Head>
 						<title>my new cool app</title>
 					</Head>
 					<Component {...pageProps} />
 					<GlobalStyles />
-				</ApolloProvider>
+				</>
 			</ThemeProvider>
 		);
 	}
 }
 
-export default withApolloClient(Application);
+export default Application;
